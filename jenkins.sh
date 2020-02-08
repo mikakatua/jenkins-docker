dea3838156d07e5bef05d99f1c23b8befa515518
Mika Katua <44744529+mikakatua@users.noreply.github.com>
2020-02-08 17:45:19 +0100

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -16,13 +16,13 @@ else
   docker run --name $NAME -d -v $VOLUME:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --group-add=$(stat -c %g /var/run/docker.sock) -p 8080:8080 -p 50000:50000 $IMAGE
   # Additional steps
   docker exec -t -u 0 $NAME bash -c "\
-  groupadd -g 999 docker && \
-  apt-get update && \
-  apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common && \
-  curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && \
-  add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian \$(lsb_release -cs) stable\" && \
-  apt-get update && \
-  apt-get -y install docker-ce-cli && \
-  usermod -aG docker jenkins"
+    groupadd -g $(stat -c %g /var/run/docker.sock) docker && \
+    apt-get update && \
+    apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common && \
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && \
+    add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian \$(lsb_release -cs) stable\" && \
+    apt-get update && \
+    apt-get -y install docker-ce-cli && \
+    usermod -aG docker jenkins"
 fi
 
